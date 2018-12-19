ee569b0f3208a6999f506a7c4869293e08658742
mehmeterim <mehmeterim1995@gmail.com>
2018-12-20 00:06:16 +0300

    Change folder structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UiModule } from '../@ui/ui.module';
-import { CoreModule } from '../@core/core.module';
+import { UiModule } from '@ui/ui.module';
+import { CoreModule } from '@core/core.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgxsModule } from '@ngxs/store';
 import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
