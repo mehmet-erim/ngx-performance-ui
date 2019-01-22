6d13643060b227df88d49dcbedce543998855037
mehmterim <bilgi@mehmeterim.com>
2019-01-22 08:47:03 +0300

    add inputs and outputs from input.component

--- src/app/@ui/components/input/input.component.ts
+++ src/app/@ui/components/input/input.component.ts
@@ -1,18 +1,43 @@
-import { Component, OnInit, ChangeDetectionStrategy, ViewEncapsulation, Input } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ChangeDetectionStrategy,
+  ViewEncapsulation,
+  Input,
+  Output,
+  EventEmitter,
+} from '@angular/core';
 import { AbstractNgModelComponent } from 'shared/abstracts/ng-model.component';
 import { Mask } from '@core/models';
 
 @Component({
   selector: 'p-input',
   template: `
-    <label class="{{ labelClasses }}" [attr.for]="inputId" [innerHTML]="labelText"></label>
+    <label class="{{ labelClasses }}" [attr.for]="id" [innerHTML]="labelText"></label>
     <input
       class="form-control {{ classes }}"
-      [attr.id]="id"
-      [attr.type]="type"
+      [(ngModel)]="value"
+      [id]="id"
+      [type]="type"
       [placeholder]="placeholder"
       [textMask]="textMask"
-      [(ngModel)]="value"
+      [hidden]="hidden"
+      [name]="name"
+      [disabled]="disabled"
+      [min]="'' + min"
+      [max]="'' + max"
+      [minlength]="minlength"
+      [maxlength]="maxlength"
+      [attr.tabindex]="tabindex"
+      [attr.autocomplete]="autocomplete"
+      [attr.autocorrect]="autocorrect"
+      [attr.spellcheck]="spellcheck"
+      (change)="onChange($event)"
+      (keyup)="keyup.emit($event)"
+      (focus)="focus.emit($event)"
+      (blur)="blur.emit($event)"
+      (click)="click.emit($event)"
+      [required]="required"
     />
     <small class="form-text text-muted {{ helpTextClasses }}" [innerHTML]="helpText"></small>
   `,
@@ -37,24 +62,51 @@ export class InputComponent extends AbstractNgModelComponent {
     return this._textMask;
   }
 
-  @Input()
-  classes: string;
+  @Input() classes: string;
 
-  @Input()
-  helpText: string;
+  @Input() helpText: string;
 
-  @Input()
-  helpTextClasses: string;
+  @Input() helpTextClasses: string;
 
-  @Input()
-  labelText: string;
+  @Input() labelText: string;
 
-  @Input()
-  labelClasses: string;
+  @Input() labelClasses: string;
 
-  @Input()
-  type: string = 'text';
+  @Input() placeholder: string;
 
-  @Input()
-  placeholder: string;
+  @Input() id: string;
+
+  @Input() name: string;
+
+  @Input() type: 'text' | 'number' | 'password' = 'text';
+
+  @Input() tabindex: number;
+
+  @Input() min: number;
+
+  @Input() max: number;
+
+  @Input() minlength: number;
+
+  @Input() maxlength: number;
+
+  @Input() required: boolean = false;
+
+  @Input() hidden: boolean = false;
+
+  @Input() autocomplete: boolean = false;
+
+  @Input() autocorrect: boolean = false;
+
+  @Input() spellcheck: boolean = false;
+
+  @Output() change = new EventEmitter();
+
+  @Output() blur = new EventEmitter();
+
+  @Output() focus = new EventEmitter();
+
+  @Output() keyup = new EventEmitter();
+
+  @Output() click = new EventEmitter();
 }
